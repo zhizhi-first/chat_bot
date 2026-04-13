ad15c575b24306dbc773ca3820d3f5df4edca4ca
zhizhi-first <19979822498@163.com>
2026-04-13 14:52:28 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationChain 
+from langchain.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain.memory import ConversationBufferMemory
+st.title("学习助手")
+with st.sidebar:
+    subject=st.selectbox(
+        "选择学科领域",
+        options=["Vue","软件设计师","English","408","SpringBoot","软件工程"],
+    )
+    style=st.selectbox(
+        "讲解风格",
+        options=["简洁","详细"],
+    )
+
+user_input=st.chat_input("你的问题/你的需求")   
+if "messages" not in st.session_state:
+    st.session_state["messages"]=[
+        {"role":"assistant","content":"你好，是你的学习助手！"},
+    ]
+    st.session_state["memory"]=ConversationBufferMemory(
+        memory_key="chat_history",return_messages=True
+    )
+for message in st.session_state["messages"]:
+    st.chat_message(message["role"]).write(message["content"]) 
+
+def get_prompt_template(subject, style):
+    style_dict = {
+        "简洁": "仅提供直接答案和最少的必要解释。不要添加额外细节、发散讨论或无关信息。保持回答清晰、简洁，目标是为用户快速提供解决方案。",
+        "详细": "第一，针对用户提问给出直接答案和清晰的解释；第二，基于此提供必要的相关知识点的信息，以补充背景或加深理解。",
+    }
+    system_template = "你是{subject}领域的专家，根据用户提问作出回答。\n你需要遵循以下讲解风格：{style}。\n你应当礼貌拒绝与该学科无关的问题。"
+    prompt_template = ChatPromptTemplate(
+        [
+            ("system", system_template),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{input}"),
+        ],
+        partial_variables={"subject": subject, "style": style_dict[style]},
+    )
+    return prompt_template
+
+def generate_response(user_input, subject, style, memory):
+    client = ChatOpenAI(
+        api_key=st.secrets["OPENAI_API_KEY"],
+        model="deepseek-chat",
+        base_url="https://api.deepseek.com",
+        temperature=0.0,
+    )
+    prompt = get_prompt_template(subject, style)
+    chain = ConversationChain(llm=client, memory=memory, prompt=prompt)
+    response = chain.invoke({"input": user_input})
+    return response["response"]
+
+if user_input:
+    st.chat_message("human").write(user_input)
+    st.session_state["messages"].append({"role": "human", "content": user_input})
+    with st.spinner("AI正在思考中，请稍等..."):
+        response = generate_response(
+            user_input, subject, style, st.session_state["memory"]
+        )
+    st.chat_message("assistant").write(response)
+    st.session_state["messages"].append({"role": "assistant", "content": response})
